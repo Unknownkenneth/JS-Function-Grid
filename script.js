ccf90048fc231be1f6087d6f2adb0aa08267b7e1
Kenneth Louis <lowkam4@gmail.com>
2017-01-09 15:01:14 +0000

    did a grid project

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 //Write your function calls below this line
+//  face
+// setColor(3, 3, "purple");
+// setColor(3, 6,"green" )
+
+// setColor(2, 8,"red" )
+
+// setColor(3, 8,"red" )
+
+// setColor(4, 8,"red" )
+
+// setColor(5, 7,"black" )
+
+// setColor(6, 6,"black" )
+
+// setColor(6, 5,"black" )
+
+// setColor(6, 4,"black" )
+
+// setColor(6, 3,"black" )
+
+// setColor(5, 2,"black" )
+
+// setColor(4, 1 ,"red" )
+
+// setColor(3, 1 ,"red" )
+
+// setColor(2, 1 ,"red" )
+
+// four corners
+// setColor(0, 0,"black" )
+
+// setColor(9, 9,"purple" )
+
+// setColor(9, 0 ,"red" )
+
+// setColor(0, 9,"pink" )
+
+
+
+
+// setColor(3, 5 ,"black" )
+
+
+
+
+// setColor(9, 5 ,"blue" )
+// setColor(8, 4 ,"blue" )
+// setColor(7, 3 ,"blue" )
+// setColor(6, 2 ,"blue" )
+// setColor(5, 1 ,"blue" )
+// setColor(9, 5 ,"blue" )
+
+
+setColor(0,  0,"gold" )
+
+setColor(,  ,"gold" )
